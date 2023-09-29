9c8ae1635463dee9cf11a02b98ead112045c8457
johnsnow09 <48548048+johnsnow09@users.noreply.github.com>
2023-09-29 15:19:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ import sys
 import wget
 import tarfile
 
-f = wget.download("http://download.oracle.com/otn-pub/java/jdk/11+28/55eed80b163941c8885ad9298e6d786a/jdk-11_linux-x64_bin.tar.gz")
-
-print(os.getcwd())
-print(os.listdir())
 
 ############### Setting Configuration ###############
 
@@ -23,6 +19,21 @@ st.set_page_config(page_title="Political Parties Crime Record Analysis",
 config = dict({'scrollZoom': False})
 
 ############### Setting Configuration Ends ###############
+
+
+os.remove("jdk-11_linux-x64_bin.tar.gz")
+#os.remove("jdk-11_linux-x64_bin.tar (1).gz")
+
+if "jdk-11_linux-x64_bin.tar.gz" not in os.listdir():
+  st.write("Starting downloading Java & execution steps")
+  f = wget.download("http://download.oracle.com/otn-pub/java/jdk/11+28/55eed80b163941c8885ad9298e6d786a/jdk-11_linux-x64_bin.tar.gz")
+
+else: 
+  st.write("Java file already exist")
+  
+print(os.getcwd())
+print(os.listdir())
+
 
 
 ############################## CREATING HEADER ##############################
@@ -41,34 +52,26 @@ with header_mid:
 ############################## HEADER DONE ##############################
 
 
-os.remove("jdk-11_linux-x64_bin.tar.gz")
-#os.remove("jdk-11_linux-x64_bin.tar (1).gz")
-
-if "jdk-11_linux-x64_bin.tar.gz" not in os.listdir():
-  st.write("Starting execution steps")
 
   ####################### Java installation #######################
-  #Name of tarfile (without the .tar.gz extension)
-  file_name = "jdk-11_linux-x64_bin"
+#Name of tarfile (without the .tar.gz extension)
+file_name = "jdk-11_linux-x64_bin"
 
-  #Target directory
-  target_dir = "mydirectory"
+#Target directory
+target_dir = "mydirectory"
 
-  #Extract the tar file
-  os.system("tar -xf " + file_name + ".tar.gz")
+#Extract the tar file
+os.system("tar -xf " + file_name + ".tar.gz")
 
-  file = tarfile.open('jdk-11_linux-x64_bin.tar.gz')
+file = tarfile.open('jdk-11_linux-x64_bin.tar.gz')
   
-  # extracting file
-  file.extractall('./mydirectory')
-  file.close()
+# extracting file
+file.extractall('./mydirectory')
+file.close()
 
-  #Move the tar files into your target folder
-  #os.system("mv " + file_name + " " + target_dir)
+#Move the tar files into your target folder
+#os.system("mv " + file_name + " " + target_dir)
 
-  st.write("Ending execution steps")
-
-else: 
-  st.write("Java file already exist")
+st.write("Ending execution steps")
   
 st.write(os.listdir())
