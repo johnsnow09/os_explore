ed95f99e4a81ec65b4b9286444f32858fa61862d
johnsnow09 <48548048+johnsnow09@users.noreply.github.com>
2023-09-29 16:30:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,16 +21,21 @@ config = dict({'scrollZoom': False})
 ############### Setting Configuration Ends ###############
 
 
-if "jdk-11_linux-x64_bin.tar.gz" not in os.listdir():
+# if "jdk-11_linux-x64_bin.tar.gz" not in os.listdir():
+if "jdk-11.0.2_linux-x64_bin.rpm" not in os.listdir():
+  
   st.write("Starting downloading Java & execution steps")
-  f = wget.download("http://download.oracle.com/otn-pub/java/jdk/11+28/55eed80b163941c8885ad9298e6d786a/jdk-11_linux-x64_bin.tar.gz")
+  #f = wget.download("http://download.oracle.com/otn-pub/java/jdk/11+28/55eed80b163941c8885ad9298e6d786a/jdk-11_linux-x64_bin.tar.gz")
+
+  # for linux from: https://crunchify.com/where-is-java-installed-on-my-mac-osx-system/
+  f = wget.download("https://download.oracle.com/otn-pub/java/jdk/11.0.2+9/f51449fcd52f4d52b93a989c5c56ed3c/jdk-11.0.2_linux-x64_bin.rpm")
   st.write(" Java Downloaded")
 
 else: 
   st.write("Java file already exist")
   
-print("Current Directory: ",os.getcwd())
-print("Directory list: ",os.listdir())
+#print("Current Directory: ",os.getcwd())
+#print("Directory list: ",os.listdir())
 
 
 
@@ -51,28 +56,28 @@ with header_mid:
 
 
 
-  ####################### Java installation #######################
+  ####################### Java installation tar.gzp file #######################
 st.write("Installing Java")
 
 #Name of tarfile (without the .tar.gz extension)
-file_name = "jdk-11_linux-x64_bin"
+#file_name = "jdk-11_linux-x64_bin"
 
 #Target directory
-target_dir = "mydirectory"
+#target_dir = "mydirectory"
 
 #Extract the tar file
-os.system("tar -xf " + file_name + ".tar.gz")
+#os.system("tar -xf " + file_name + ".tar.gz")
 
-file = tarfile.open('jdk-11_linux-x64_bin.tar.gz')
+#file = tarfile.open('jdk-11_linux-x64_bin.tar.gz')
   
 # extracting file
-file.extractall('./mydirectory')
-file.close()
+#file.extractall('./mydirectory')
+#file.close()
 
 #Move the tar files into your target folder
 #os.system("mv " + file_name + " " + target_dir)
 
-st.write("Ending execution steps")
+#st.write("Ending execution steps")
 
 os.remove("jdk-11_linux-x64_bin.tar.gz")
 #os.remove("jdk-11_linux-x64_bin.tar (1).gz")
