d057b78bf8a0d58df0fa56de23f1a8b21e25fa31
johnsnow09 <48548048+johnsnow09@users.noreply.github.com>
2023-09-29 13:18:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,21 @@ import sys
 
 print(os.getcwd())
 print(os.listdir())
+
+
+
+
+############### Setting Configuration ###############
+
+st.set_page_config(page_title="Political Parties Crime Record Analysis",
+                    layout='wide',
+                    initial_sidebar_state="expanded")
+
+
+# Setting configuration to diable plotly zoom in plots
+config = dict({'scrollZoom': False})
+
+############### Setting Configuration Ends ###############
+
+
+st.write(os.getcwd())
